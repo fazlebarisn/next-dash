e370245262f866dc65701239fb8073429e9c2b0a
Fazle Bari <fazlebarisn@gmail.com>
2023-11-17 14:13:37 +0600

    menu link added

---
+++ app/ui/dashboard/sidebar/menuLink/menuLink.jsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import styles from "./menuLink.module.css"
+
+const menuLink = ({item}) => {
+    return(
+        <Link href={item.path} className={styles.container}>
+            {item.icon}
+            {item.title}
+        </Link>
+    )
+}
+export default menuLink
